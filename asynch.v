ff65f097225819ef7596902223f3a386104b0960
navyachiluka <116788280+navyachiluka@users.noreply.github.com>
2024-05-22 09:06:30 -0700

    Update asynch.v

--- asynch.v
+++ asynch.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Chiluka Navya
 // 
 // Create Date: 16.04.2024 18:48:34
 // Design Name: 
 // Module Name: asynch
-// Project Name: 
+// Project Name: ASYNCHRONOUS COUNTER
 // Target Devices: 
-// Tool Versions: 
+// Tool Versions: VIVADO
 // Description: 
 // 
 // Dependencies: 
@@ -19,11 +19,11 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+//creating module named asynch
 module asynch(
-input clk,rst,
-output Q0,Q1,Q2,Q3);
-jk_ff g1(clk,1'b1,1'b1,rst,Q0);
+input clk,rst,//declaration of inputs
+  output Q0,Q1,Q2,Q3);//declaratiion of outputs
+  jk_ff g1(clk,1'b1,1'b1,rst,Q0);//calling or instantiating jk flip flop
 jk_ff g2(Q0,1'b1,1'b1,rst,Q1);
 jk_ff g3(Q1,1'b1,1'b1,rst,Q2);
 jk_ff g4(Q2,1'b1,1'b1,rst,Q3);
